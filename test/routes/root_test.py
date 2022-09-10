3fb5d49a5e4ccf3d29ddd70622b6e1eed060ed33
Sunny <54858282+sunnydonaldson@users.noreply.github.com>
2022-09-10 16:47:45 +0100

    Remove Flask Stuff.

--- test/routes/root_test.py
+++
@@ -1,6 +0,0 @@
-from test.helpers.server_test_base import ServerTestBase
-
-class TestRoutes(ServerTestBase):
-  def test_root_route(self, client):
-    response = client.get("/")
-    assert(response.status_code == 501)
