26b3862356cb921e1168f2fbcd284a02503d961d
jacob.eva <x00010@disroot.org>
2022-12-27 21:14:16 +0000

    Added rocket server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod connections;
+use connections::ldap;
+mod settings;
+use settings::LdapConfig;
+
+#[macro_use] extern crate rocket;
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(LdapConfig::new())
 }
