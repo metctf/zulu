d734e8794c10295dc1418bc920c694094ef1cafe
jacob.eva <x00010@disroot.org>
2023-02-13 12:57:50 +0000

    Corrected capitalisation

--- zulu/zulu.sql
+++ zulu/zulu.sql
@@ -8,35 +8,35 @@ GRANT ALL ON zulu.* TO zulu@localhost;
 
 DROP TABLE IF EXISTS zulu.accounts;
 CREATE TABLE zulu.accounts (
-    accountID uuid DEFAULT UUID() PRIMARY KEY,
+    accountid uuid DEFAULT UUID() PRIMARY KEY,
     username varchar(50) NOT NULL,
-    firstName varchar(50) NOT NULL,
-    lastName varchar(50) NOT NULL,
+    firstname varchar(50) NOT NULL,
+    lastname varchar(50) NOT NULL,
     password varchar(255),
 	origin varchar(50) NOT NULL,
-    flagQuantity int(11) UNSIGNED DEFAULT 0 NOT NULL,
-    accessLevel varchar(10) NOT NULL,
-	creationTime timestamp NOT NULL
+    flagquantity int(11) UNSIGNED DEFAULT 0 NOT NULL,
+    accesslevel varchar(10) NOT NULL,
+	creationtime timestamp NOT NULL
 ); 
 
 
 DROP TABLE IF EXISTS zulu.flags;
 CREATE TABLE zulu.flags (
-    flagID uuid DEFAULT uuid() PRIMARY KEY,
+    flagid uuid DEFAULT uuid() PRIMARY KEY,
     challenge varchar(255) NOT NULL,
-    challengeAuthor varchar(255) NOT NULL,
-    flagString varchar(255) NOT NULL,
+    challengeauthor varchar(255) NOT NULL,
+    flagstring varchar(255) NOT NULL,
     points int(11) UNSIGNED DEFAULT 0 NOT NULL,
-	creationTime timestamp NOT NULL
+	creationtime timestamp NOT NULL
 ); 
 
 
 DROP TABLE IF EXISTS zulu.accountFlags;
 CREATE TABLE zulu.accountFlags (
-    fkAccountID uuid DEFAULT uuid(),
-    fkFlagID uuid DEFAULT uuid(),
-    creationTime timestamp NOT NULL,
-    CONSTRAINT accountFlags_ibfk_1 FOREIGN KEY (fkAccountID) REFERENCES accounts (accountID),
-    CONSTRAINT accountFlags_ibfk_2 FOREIGN KEY (fkFlagID) REFERENCES flags (flagID),
-    PRIMARY KEY(fkAccountID, fkFlagID)
+    fkaccountid uuid DEFAULT uuid(),
+    fkflagid uuid DEFAULT uuid(),
+    creationtime timestamp NOT NULL,
+    CONSTRAINT accountflags_ibfk_1 FOREIGN KEY (fkaccountid) REFERENCES accounts (accountid),
+    CONSTRAINT accountFlags_ibfk_2 FOREIGN KEY (fkFlagID) REFERENCES flags (flagid),
+    PRIMARY KEY(fkaccountid, fkflagid)
 );
