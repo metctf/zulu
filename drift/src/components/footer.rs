0c8f1fb093e3971f9d1c1a26bd629c7edcdc2acb
Zeos-ctrl <astrilcodex@gmail.com>
2023-03-06 11:15:22 +0000

    fixed slider bug

--- drift/src/components/footer.rs
+++ drift/src/components/footer.rs
@@ -3,8 +3,29 @@ use gloo::utils::document;
 use gloo_storage::LocalStorage;
 use gloo::storage::Storage;
 
+fn theme_select() -> bool {
+    let theme: Result<String,_> = LocalStorage::get("theme");
+
+    // Check what the theme is a returned value of false is light mode 
+    // and true is dark mode
+    match theme {
+        Ok(theme) => {
+            if theme.eq("light") {
+                false
+            } else {
+                true 
+            }
+        },
+        Err(_) => {
+            false
+        }
+    }
+}
+
 #[function_component(Footer)]
 pub fn footer() -> Html {
+    let is_checked: bool = theme_select();
+
     let on_click = Callback::from(|_: MouseEvent| {
             let root_element = document().document_element().unwrap();
             let theme = root_element.get_attribute("theme").unwrap();
@@ -18,14 +39,16 @@ pub fn footer() -> Html {
             }
             root_element.set_attribute("theme", &new_theme).unwrap();
             LocalStorage::set("theme", &new_theme).unwrap();
+
     });
+
 
     html! {
         <div class={classes!("footer")}>
             <p class={classes!("label")}>{"Light"}</p>
             <label class={classes!("switch", "label")}>
-                <input type="checkbox" onclick={on_click} />
-                <span class="slider round" id="slider"></span>
+                <input type="checkbox" checked={is_checked} onclick={on_click} />
+                <span class="slider round" ></span>
             </label>
             <p class={classes!("label")}>{"Dark"}</p>
             <a href="https://github.com/metctf/zulu" class={classes!("zulu-github")}>{"Zulu CardiffMet CTF Society 2023 "}</a>
